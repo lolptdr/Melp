8a6b37e0fefdabf85e982e52f9f607e0bb45ec6b
Joseph Tingsanchali <jtingsan@gmail.com>
2016-05-30 09:51:42 -0500

    Add font-awesome css

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import 'font-awesome/css/font-awesome.css'
 import './app.css'
 import styles from './styles.module.css'
 
@@ -8,11 +9,11 @@ const App = React.createClass({
   render: function() {
     return (
       <div className={styles.wrapper}>
-        Text text text
-
-        <h1>Environment: {__NODE_ENV__}</h1>
+        <h1>
+          <i className="fa fa-star"></i>
+          Environment: {__NODE_ENV__}</h1>
       </div>
-    );
+    )
   }
 });
 
