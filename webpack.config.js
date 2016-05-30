508f8c503fd4602d47783dd3388b28e8e33a37b5
Joseph Tingsanchali <jtingsan@gmail.com>
2016-05-30 01:30:48 -0500

    Add precss, autoprefixer, cssnano CSS modules

--- webpack.config.js
+++ webpack.config.js
@@ -24,4 +24,10 @@ var config = getConfig({
   clearBeforeBuild: true
 });
 
+config.postcss = [].concat([
+  require('precss')({}),
+  require('autoprefixer')({}),
+  require('cssnano')({})
+])
+
 module.exports = config;
