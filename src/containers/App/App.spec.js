d4c14bb77786c56c7a47e36978d4e529fe3610cf
Joseph Tingsanchali <jtingsan@gmail.com>
2016-06-01 18:34:49 -0500

    Set up initial test for App container and Router component

--- src/containers/App/App.spec.js
+++ src/containers/App/App.spec.js
@@ -3,16 +3,17 @@ import { expect } from 'chai'
 import { shallow } from 'enzyme'
 
 import App from './App'
-import styles from './styles.module.css'
 
-describe('<App />', function () {
+describe('<App />', () => {
+
   let wrapper;
   beforeEach(() => {
-    wrapper = shallow(<App />)
-  })
-
-  it('has a single wrapper element', () => {
-    expect(wrapper.find(`.${styles.wrapper}`))
-            .to.have.length(1);
+    wrapper = shallow(<App />);
   });
+
+  it('has a Router component', () => {
+    expect(wrapper.find('Router'))
+      .to.have.length(1);
+  });
+
 });
